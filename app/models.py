080e831de4a3b8fed9bf1fc227c06de0cd22dd15
Danil Tumanov <dania.tumanov@gmail.com>
2022-06-19 10:46:22 +0300

    commit

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class GroupModel(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+
+class StudentModel(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    group_id = Column(Integer, ForeignKey('groups.id'), nullable=False)
+    first_name = Column(String(20))
+    last_name = Column(String(20))
+
+
+class CourseModel:
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
