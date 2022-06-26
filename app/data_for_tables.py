22f161c0d27d3de70bf5b4af7e9501c1bd993773
Danil Tumanov <dania.tumanov@gmail.com>
2022-06-26 15:39:41 +0300

    commit

--- app/data_for_tables.py
+++ app/data_for_tables.py
@@ -2,9 +2,9 @@ from random import choice, randint
 import string
 
 first_names = ["Aren", "Arez", "Arman", "Bryan", "Bryce", "Bryden",
-         "Darn", "Darrach", "Darragh", "Darrel", "Dan", "Darren",
-         "Taliesin", "Talon", "Talorcan", "Tamar", "Tamiem", "Tammam",
-         "Zubayr", "Zuriel"]
+               "Darn", "Darrach", "Darragh", "Darrel", "Dan", "Darren",
+               "Taliesin", "Talon", "Talorcan", "Tamar", "Tamiem", "Tammam",
+               "Zubayr", "Zuriel"]
 
 last_names = ['Abbott', 'Acevedo', 'Acosta', 'Adams', 'Adkins', 'Aguilar', 'Black', 'Blackburn', 'Blackwell', 'Blair',
               'Blake', 'Blanchard', 'Blankenship', 'Blevins',  'Fox', 'Francis', 'Franco', 'Frank', 'Franklin',
@@ -14,11 +14,11 @@ courses = ['math', 'biology', 'english', 'chemistry', 'football', 'basketball', 
 
 
 def create_random_groups(number=10):
-        letters = string.ascii_lowercase
-        groups = []
-        for i in range(number):
-                groups.append(f'{choice(letters)}{choice(letters)}-{randint(10, 99)}')
-        return groups
+    letters = string.ascii_lowercase
+    groups = []
+    for i in range(number):
+        groups.append(f'{choice(letters)}{choice(letters)}-{randint(10, 99)}')
+    return groups
 
 
 groups = create_random_groups()
